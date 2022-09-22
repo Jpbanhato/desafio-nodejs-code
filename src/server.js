243430cf839aad01f5f3aafb88a5880c7a51751b
Joao Pedro Banhato <jpbbanhato@gmail.com>
2022-09-22 00:16:18 -0300

    Inicia busca deptos

--- src/server.js
+++ src/server.js
@@ -8,10 +8,36 @@ app.use(cors({
   origin: "*"
 }));
 
+const deptos = ['VPGG', 'Comercial', 'Marketing', 'Presidencia', 'Projetos'];
 
-app.get('/', (req, res) => {
-  console.log("GET /");
-  res.status(200).send("Hello World");
+app.get('/deptos', (req, res) => {
+  // console.log("GET /");
+  // res.status(200).send("Hello World");
+  return res.json(deptos);
+});
+
+app.get('/deptos/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json(deptos[index]);
+});
+
+app.post('/deptos', (req, res) => {
+  const {name} = req.body;
+  deptos.push(name);
+  return res.json(deptos);
+});
+
+app.put('/deptos/:index', (req, res) => {
+  const {index} = req.params;
+  const {name} = req.body;
+  deptos[index] = name;
+  return res.json(deptos);
+});
+
+app.delete('/deptos/:index', (req, res) => {
+  const {index} = req.params;
+  deptos.splice(index, 1);
+  return res.json({message: "Depto deletado com sucesso"});
 });
 
 app.listen('3333', () => {
